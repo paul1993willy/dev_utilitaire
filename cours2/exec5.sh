8c9722f6835c4ffd7cb82df4339f183e6250c0fc
Paul-Willy Jean <paul1993willy@gmail.com>
2013-03-07 21:05:37 -0500

    Finit le script #5

--- cours2/exec5.sh
+++ cours2/exec5.sh
@@ -6,17 +6,39 @@ if [[ $# != 1 ]]; then
 	exit 1
 fi
 
+fichier=$1
+
 # Récupère le UID du propriétaire fichier ou du répertoire à vérifier
-own=$(stat --format="%u" $1)
+own=$(stat --format="%u" $fichier)
 
 # Récupère les permission de l'utilisateur sur le fichier.
 
 echo "Nom de l'utilisateur lançant le script : $USER"
 
 if [[ $own == $UID ]]; then
-	echo "$1 appartient à $USER"
+	echo "\"$fichier\" appartient à $USER"
 else
-	echo "$1 n'appartient pas à $USER"
+	echo "$fichier n'appartient pas à $USER"
 fi
 
 echo -n "Vous avez le droit de "
+
+if [ -w $fichier ]; then
+	echo -n "écriture"
+fi
+
+if [ -r $fichier ]; then
+	if [ -w $fichier ]; then
+		echo -n "/"
+	fi
+	echo -n "lecture"
+fi
+
+if [ -x $fichier ]; then
+	if [ -w $fichier -o -r $fichier ]; then
+		echo -n "/"
+	fi
+	echo -n "exécution"
+fi
+
+echo ""
