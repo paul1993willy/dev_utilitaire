691bbcc8015ef6ca3f3a09a849371b21a2beee23
Paul-Willy Jean <paul1993willy@gmail.com>
2013-04-15 20:11:03 -0400

    Preparation du code
    - Definit une nouveau IFS
    - Attribut mes premiers parametres a des variables plus lisibles

--- tp1/1022680_tp1.bash
+++ tp1/1022680_tp1.bash
@@ -8,12 +8,24 @@
 #
 ################################################################################
 
-echo "\"hello\""
+# Sauvegarde de mon Internal Field Separator
+OLD_IFS=$IFS
+
+# Je vais séparer des virgules
+IFS=","
+
 # Affiche un message d'erreur si aucun fichier ne peut être lu
 if [[ $# != 2 || ! -r $2 ]]; then
 	echo "Usage : $0 NOM_DU_GROUPE FICHIER_A_LIRE";
 	exit
 fi
+
+# Rend mes variables de groupe et de fichier plus facile à identifier et me
+# d'un set mal placé
+groupe=$1;
+fichier=$2;
+
+declare -l ligne;
 
 while read ligne; do
 	typeset -l ligne;
