9185a3ac554ad323844c3b976fabd52207315e76
Paul-Willy Jean <paul1993willy@gmail.com>
2013-04-19 11:47:01 -0400

    Travaille la formation des noms d'utilisateur

--- tp1/1022680_tp1.bash
+++ tp1/1022680_tp1.bash
@@ -22,9 +22,33 @@ fichier=$2;
 declare -l ligne;
 
 while read ligne; do
-	# Traduction des virgules en underscore et retrait des accents
-	ligne=$(echo ${ligne//,/_} | iconv -f utf8 -t ascii//TRANSLIT);
+	# Retrait des accents et des espaces
+	ligne=$(echo ${ligne// /} | iconv -f utf8 -t ascii//TRANSLIT);
 
-	# Place la ligne dans un tableau et retraduction des underscore en virgules
-	compte=(${ligne//_/,});
+	# Place la ligne dans un tableau
+	compte=($(echo ${ligne} | cut --fields="1-" --delimiter="," --output-delimiter=" "));
+
+	char_nom_famille=5;
+	char_prenom=2;
+
+	# Pour choisir un nom d'utilisateur
+	while true; do
+	#while [[ $(id $user 2> /dev/null | wc -l) == 0 ]]; do
+		# Trouve un nom au compte utilisateur
+		user="${compte[2]:0:$char_prenom}${compte[1]:0:$char_nom_famille}"
+
+		echo "USER: ${user}, FIRST_NAME: ${compte[2]}, LAST_NAME: ${compte[1]}";
+
+		# Ajoute un caractère au nom d'utilisateur jusqu'à ce qu'il soit unique
+		if [[ $((${#compte[1]} - 1)) > $char_nom_famille ]]; then
+			((char_nom_famille++));
+		elif [[ $((${#compte[2]} - 1)) > $char_prenom ]]; then
+			((char_prenom++));
+		else
+			break;
+		fi
+	done
+
+
+
 done < $fichier
