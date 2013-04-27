276834bc66a38b5985590da58d4f193bf5d0d84c
Paul-Willy Jean <paul1993willy@gmail.com>
2013-04-27 17:54:59 -0400

    Teste la creation du nom d'utilisateur

--- tp1/1022680_tp1.bash
+++ tp1/1022680_tp1.bash
@@ -60,15 +60,17 @@ while read ligne; do
 		# Trouve un nom au compte utilisateur
 		user="${compte[2]:0:$char_prenom}${compte[1]:0:$char_nom_famille}"
 
-		echo "USER: ${user}, FIRST_NAME: ${compte[2]}, LAST_NAME: ${compte[1]}";
-
 		# Ajoute un caractère au nom d'utilisateur jusqu'à ce qu'il soit unique
 		if [[ $((${#compte[1]} - 1)) > $char_nom_famille ]]; then
+			echo "Je suis ${compte[1]}, un nom de famille, j'ai ${#compte[1]} caracteres et suis coupe a $char_nom_famille";
 			((char_nom_famille++));
-		elif [[ $((${#compte[2]} - 1)) > $char_prenom ]]; then
+		elif [[ $((${#compte[0]} - 1)) > $char_prenom ]]; then
+			echo "Je suis ${compte[0]}, un prenom, j'ai ${#compte[2]} caracteres et suis coupe a $char_prenom";
 			((char_prenom++));
 		else
+			echo "Je suis ${compte[*]} On a expire ma longueur totale";
 			break;
 		fi
 	done
 done < $fichier
+
