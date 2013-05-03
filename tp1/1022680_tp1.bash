749e384fcbe48f663612f62c9d58827428d2836b
Paul-Willy Jean <paul1993willy@gmail.com>
2013-05-03 09:10:22 -0400

    Retrait du code de debogage

--- tp1/1022680_tp1.bash
+++ tp1/1022680_tp1.bash
@@ -12,23 +12,12 @@ function strip_accent ()
 {
 	# Retrait des accents et des espaces
 	ligne=$(echo ${ligne//\ /} | iconv -f utf8 -t ascii//TRANSLIT)
-
-	#
-	# Test
-	#
-	# echo ${ligne}
 }
 
 function couper_ligne ()
 {
 	# Place la ligne dans un tableau
 	compte=($(echo ${ligne} | cut --fields="1-" --delimiter="," --output-delimiter=" "));
-
-	#
-	# Test
-	#
-	# IFS=";"$IFS
-	# echo "${compte[*]}"
 }
 
 # Affiche un message d'erreur si aucun fichier ne peut être lu
@@ -55,17 +44,11 @@ while read ligne; do
 	# Reduit le nombre de caracteres de l'offset s'il est plus long que la chaine
 	if [[ ${#compte[2]} < $char_prenom ]]; then
 		char_prenom=${#compte[2]}
-		# echo -e "${#compte[2]}\v${compte[2]}"
-		# echo -n "${compte[2]};"
-		# echo "$char_prenom"
 	fi
 
 	# Reduit le nombre de caracteres de l'offset s'il est plus long que la chaine
 	if [[ ${#compte[1]} < $char_nom_famille ]]; then
 		char_nom_famille=${#compte[1]}
-		# echo -n "${#compte[1]};"
-		# echo -n "${compte[1]};"
-		# echo "$char_nom_famille'
 	fi
 
 	# Trouve un nom au compte utilisateur
